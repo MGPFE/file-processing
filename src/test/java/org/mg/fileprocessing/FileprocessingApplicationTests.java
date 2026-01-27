4e4ca6fdb1d0ddb5dbb6b8e1fe4aec8e8ebb9fb8
Mariusz <mariuszgworek99@gmail.com>
2026-01-27 21:21:19 +0100

    Fixed contextLoads test

--- src/test/java/org/mg/fileprocessing/FileprocessingApplicationTests.java
+++ src/test/java/org/mg/fileprocessing/FileprocessingApplicationTests.java
@@ -3,9 +3,11 @@ package org.mg.fileprocessing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @EmbeddedKafka
+@ActiveProfiles("test")
 class FileprocessingApplicationTests {
 	@Test
 	void contextLoads() {
